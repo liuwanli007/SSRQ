a147c374848638c6aadb98ad64b62e85c995e0ce
liuwanli007 <lwlxhxyz@163.com>
2017-06-01 09:32:34 +0800

    Add files via upload

---
+++ scripts/GameMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameMenu : MonoBehaviour {
+
+    public void OnStart()
+    {
+        //SceneManager.LoadScene(1);
+        Application.LoadLevel(1);
+    }
+    public void First()
+    {
+        //SceneManager.LoadScene(1);
+        Application.LoadLevel(2);
+    }
+    public void Second()
+    {
+        Application.LoadLevel(3);
+    }
+    public void Thired()
+    {
+        Application.LoadLevel(4);
+    }
+    public void Forth()
+    {
+        Application.LoadLevel(0);
+    }
+    public void OnExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+    }
+}
